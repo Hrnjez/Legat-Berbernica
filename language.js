b3d2e703b571c20ba243ab423c257c3995dd35c6
Hrnjez <h.nemanja@yahoo.com>
2020-09-22 22:27:30 +0200

    first commit

---
+++ language.js
@@ -0,0 +1,137 @@
+$(document).ready(function () {
+
+    const serbianLanguage = {
+        glavna: 'Glavna',
+        oNama: 'O nama',
+        cene: 'Cene',
+        rezervacija: 'Rezervacija',
+        kontakt: 'Kontakt',
+        logoTxt: 'Logo',
+        slogan: 'Ovde neki slogan',
+        bradaIFriz: 'Brada i Frizura',
+        bradaIFrizP: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, tenetur alias, expedita maxime cum iusto dignissimos dolorem queducimus recusandae facilis numquam autem consequatur nulla quiofficia quis dolores necessitatibus harum!',
+        oNamaH1: ' O nama',
+        oNamaP: "Berbernica Legat se nalazi u jednom od najlep{ih delova  Vra<span class='small-letter'>^</span>ara, u samom srcu Beograda. Legat nije obi<span class='small-letter'>^</span>na berberbnica, to je klub u kome }e svaki gospodin u`ivati u komforu entrerijera fino}i pi}a i visokom kvalitetu usluga. Verujemo da svaki mu{karac treba da nosi frizuru koja pristaje wegovom karakteru, stilu i na<span class='small-letter'>^</span>inu `ivota. Bilo da `elite klasi<span class='small-letter'>^</span>nu frizuru '50-ih,  <span class='eng-word'>fade</span> sa bezbroj varijacija ili neprevaziđeni <span class='eng-word'>rockabilly</span> stil,na{i berberi }e biti na visini zadatka. Negujemo tradiciju dok pratimo novitete. Poredfrizura, tu smo da se postaramo i za izgled Va{e brade i brkova. Verujemo u kvalitet i nastojimo da vam pru`imo prvoklasnu uslugu. Britva je na{ omiljeni alat, zato je brijawe na{a u`a specijalnost.",
+        kontaktH3: 'Kontakt',
+        kontaktP: 'Posetite nas na na{oj adresi',
+        kontaktH4: 'Adresa:',
+        vidiNaMapi: 'Vidi na mapi',
+        radnoVreme: 'Radno vreme',
+        radnoVremePonPet: 'Ponedeqak - Petak ..11:00 - 21:00',
+        radnoVremeSub: 'Subota .............10:00 - 16:00',
+        rezBtn: 'Rezervacija'
+
+      }
+    
+    const englishLanguage = {
+        glavna: 'Home',
+        oNama: 'About',
+        cene: 'Prices',
+        rezervacija: 'Reservation',
+        kontakt: 'Contact',
+        logoTxt: 'Logo',
+        slogan: 'Here is a ...',
+        bradaIFriz: 'Beard and Hairstyle',
+        bradaIFrizP: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, tenetur alias, expedita maxime cum iusto dignissimos dolorem queducimus recusandae facilis numquam autem consequatur nulla quiofficia quis dolores necessitatibus harum!',
+        oNamaH1 : 'About Us',
+        oNamaP: 'Тhe barbershop “Legacy”  is located in one of the most beautiful parts of Vracar, in the heart of Belgrade.The “Legacy” is not an ordinary barbershop, it is a club where every gentleman can enjoy in the comfort of the interior, fine liquor  and the high quality services. We believe that every man should wear a hairstyle that suits his character, and lifestyle. Whether you want a classic ’50s hairstyle, a fade, with countless variations, or a superior rockabilly style, our barbers will be up to the task. While we respect the old school, we keep up with new trends.Aside from hairstyling, we are here to take care of your beard and mustache.  We belive in quality, аnd we take our  time to ensure you get the best haircut and shave possible. The razor is our favorite tool,therefore, shaving is our specialty.',
+        kontaktH3: 'Contact',
+        kontaktP: 'Visit us at our address',
+        kontaktH4: 'Adress:',
+        vidiNaMapi: 'See Map',
+        radnoVreme: 'Working time',
+        radnoVremePonPet: 'Monday - Friday ..11:00 - 21:00',
+        radnoVremeSub: 'Saturday .............10:00 - 16:00',
+        rezBtn: 'Reservation'
+    }
+    
+    $('#srb').on('click',function(){
+        $('#btnGlavna').text(`${serbianLanguage.glavna}`);
+        $('#btnONama').text(`${serbianLanguage.oNama}`);
+        $('#btnCene').text(`${serbianLanguage.cene}`);
+        $('#btnRez').text(`${serbianLanguage.rezervacija}`);
+        $('#btnKontakt').text(`${serbianLanguage.kontakt}`);
+        $('#navBar button').css('font-family','font');
+        $('#navBar button').css('font-size','16px');
+        $('#slogan').html(`
+        <img src="img/legat-logo.png" alt="" />
+        <h4 id="legat">${serbianLanguage.logoTxt}</h4>
+        <h2>${serbianLanguage.slogan}</h2>
+        <div id="tekst">
+          <h3>${serbianLanguage.bradaIFriz}</h3>
+          <p>
+            ${serbianLanguage.bradaIFrizP}
+          </p>
+        </div>
+        `)
+        $('#navBar').css('padding','10px 3px');
+        $('#oNamaTekst h1').html(`<h1>${serbianLanguage.oNamaH1}</h1>`);
+        $('#oNamaTekst p').html(`<p>${serbianLanguage.oNamaP}</p>`);
+        $('#oNamaTekst p').css('line-height','24px');
+        $('#boxKontakt').html(` 
+        <h3>${serbianLanguage.kontaktH3}</h3>
+        <p>${serbianLanguage.kontaktP}</p>
+        <h4>${serbianLanguage.kontaktH4}</h4>
+        <p>Cara Nikolaja 2-86. Beograd, Srbija</p>
+        <button id="vidiNaMapi">
+          <a href="https://www.google.com/maps/place/Cara+Nikolaja+II+86,+Beograd/@44.7980169,20.4830692,18.67z/data=!4m5!3m4!1s0x475a70779cd6ef27:0x117c4bc8ac219f3d!8m2!3d44.7980837!4d20.4836535"
+            target="_blank">${serbianLanguage.vidiNaMapi}</a>
+        </button>
+        <h4>${serbianLanguage.radnoVreme}</h4>
+        <p>${serbianLanguage.radnoVremePonPet}</p>
+        <p>${serbianLanguage.radnoVremeSub}</p>
+        <div id="kontaktBtn">
+          <button id="rezBtn">${serbianLanguage.rezBtn}</button>
+        </div>`);
+    });
+    $('#eng').on('click',function(){
+        $('#btnGlavna').text(`${englishLanguage.glavna}`);
+        $('#btnONama').text(`${englishLanguage.oNama}`);
+        $('#btnCene').text(`${englishLanguage.cene}`);
+        $('#btnRez').text(`${englishLanguage.rezervacija}`);
+        $('#btnKontakt').text(`${englishLanguage.kontakt}`);
+        $('#navBar button').css('font-family','OSC');
+        $('#navBar button').css('font-size','15px');
+        $('#slogan').html(`
+        <img src="img/legat-logo.png" alt="" />
+        <h4 id="legat">${englishLanguage.logoTxt}</h4>
+        <h2 id="logoH2">${englishLanguage.slogan}</h2>
+        <div id="tekst">
+          <h3>${englishLanguage.bradaIFriz}</h3>
+          <p>
+            ${englishLanguage.bradaIFrizP}
+          </p>
+        </div>
+        `)
+        $('#navBar').css('padding','8px 3px');
+        $('#legat').css('font-size','16px');
+        $('#legat').css('letter-spacing','4px');
+        $('#tekst h3').css('margin','0px');
+        $('#tekst p').css('line-height','16px');
+        $('#logoH2').css('font-size','40px');
+        $('#slogan h2, #slogan h4, #tekst h3, #tekst p').css('font-family','OSC');
+        $('#oNamaTekst h1').html(`<h1>${englishLanguage.oNamaH1}</h1>`);
+        $('#oNamaTekst p').html(` <p> ${englishLanguage.oNamaP}</p>`);
+        $('#oNamaTekst h1').css('font-family','OSC');
+        $('#oNamaTekst h1').css('font-size','40px');
+        $('#oNamaTekst p').css('font-family','OSC');
+        $('#oNamaTekst p').css('line-height','27px');
+        $('#boxKontakt').html(` 
+        <h3>${englishLanguage.kontaktH3}</h3>
+        <p>${englishLanguage.kontaktP}</p>
+        <h4>${englishLanguage.kontaktH4}</h4>
+        <p>Cara Nikolaja 2-86. Beograd, Srbija</p>
+        <button id="vidiNaMapi">
+          <a href="https://www.google.com/maps/place/Cara+Nikolaja+II+86,+Beograd/@44.7980169,20.4830692,18.67z/data=!4m5!3m4!1s0x475a70779cd6ef27:0x117c4bc8ac219f3d!8m2!3d44.7980837!4d20.4836535"
+            target="_blank">${englishLanguage.vidiNaMapi}</a>
+        </button>
+        <h4>${englishLanguage.radnoVreme}</h4>
+        <p>${englishLanguage.radnoVremePonPet}</p>
+        <p>${englishLanguage.radnoVremeSub}</p>
+        <div id="kontaktBtn">
+          <button id="rezBtn">${englishLanguage.rezBtn}</button>
+        </div>`);
+        $('#boxKontakt h3,#boxKontakt h4, #boxKontakt p, #boxKontakt a, #rezBtn').css('font-family','OSC');
+        $('#boxKontakt h3,#boxKontakt h4, #boxKontakt p, #boxKontakt a, #rezBtn').css('line-height','17px');
+    });
+});
---
+++ main.js
@@ -0,0 +1,243 @@
+//Buttons-Events iz NavBar-a
+$(document).ready(function () {
+  document.getElementById("btnGlavna").addEventListener("click", glavna);
+  function glavna() {
+    var viewGlavna = document.getElementById("glavna");
+    viewGlavna.scrollIntoView({ block: "start", behavior: "smooth" });
+    console.log("Glavna");
+    $("#glavna").show(1000);
+    $("#oNama").show(1000);
+    $("#nasiRadovi").show(1000);
+    $("#cenovnik").show(1000);
+    $("#zakazivanje").show(1000);
+    $("#footer").show(1000);
+    $("#terminiDemo").hide();
+    $("#login").hide(1000);
+    $("#addImg").hide(1000);
+  }
+  document.getElementById("btnONama").addEventListener("click", oNama);
+  function oNama() {
+    var viewONama = document.getElementById("oNama");
+    viewONama.scrollIntoView({ block: "start", behavior: "smooth" });
+    $("#glavna").show(1000);
+    $("#oNama").show(1000);
+    $("#nasiRadovi").show(1000);
+    $("#cenovnik").show(1000);
+    $("#zakazivanje").show(1000);
+    $("#footer").show(1000);
+    $("#terminiDemo").hide();
+    $("#login").hide(1000);
+    $("#addImg").hide(1000);
+    console.log("O nama");
+  }
+  document.getElementById("btnCene").addEventListener("click", cene);
+  function cene() {
+    var viewCene = document.getElementById("cenovnik");
+    viewCene.scrollIntoView({ block: "start", behavior: "smooth" });
+    $("#glavna").show(1000);
+    $("#oNama").show(1000);
+    $("#nasiRadovi").show(1000);
+    $("#cenovnik").show(1000);
+    $("#zakazivanje").show(1000);
+    $("#footer").show(1000);
+    $("#terminiDemo").hide();
+    $("#login").hide(1000);
+    $("#addImg").hide(1000);
+    console.log("Cene");
+  }
+  document.getElementById("btnRez").addEventListener("click", rezervacija);
+  function rezervacija() {
+    var viewZakazivanje = document.getElementById("zakazivanje");
+    viewZakazivanje.scrollIntoView({ block: "start", behavior: "smooth" });
+    $("#glavna").show(1000);
+    $("#oNama").show(1000);
+    $("#nasiRadovi").show(1000);
+    $("#cenovnik").show(1000);
+    $("#zakazivanje").show(1000);
+    $("#footer").show(1000);
+    $("#login").hide(1000);
+    $("#addImg").hide(1000);
+    $("#terminiDemo").hide();
+    console.log("Rezervacija");
+  }
+  document.getElementById("btnKontakt").addEventListener("click", kontakt);
+  function kontakt() {
+    var viewKontakt = document.getElementById("footer");
+    viewKontakt.scrollIntoView({ block: "start", behavior: "smooth" });
+    $("#glavna").show(1000);
+    $("#oNama").show(1000);
+    $("#nasiRadovi").show(1000);
+    $("#cenovnik").show(1000);
+    $("#zakazivanje").show(1000);
+    $("#footer").show(1000);
+    $("#terminiDemo").hide();
+    $("#login").hide(1000);
+    $("#addImg").hide(1000);
+    console.log("Kontakt");
+  }
+  document.getElementById("rezBtn").addEventListener("click", rezervacijaBtn);
+  function rezervacijaBtn() {
+    var viewZakazivanje = document.getElementById("zakazivanje");
+    viewZakazivanje.scrollIntoView({ block: "start", behavior: "smooth" });
+    $("#glavna").show(1000);
+    $("#oNama").show(1000);
+    $("#nasiRadovi").show(1000);
+    $("#cenovnik").show(1000);
+    $("#zakazivanje").show(1000);
+    $("#footer").show(1000);
+    $("#terminiDemo").hide();
+    $("#login").hide(1000);
+    $("#addImg").hide(1000);
+    console.log("Rezervacija");
+  }
+
+  // Zakazivanje termina jQuery
+  $("#terminiDemo").hide();
+  $("#zakazivanje p").hide();
+  $("#inputBoxes").hide();
+  $("#zakazi").hide();
+  $("#rezTermini").hide();
+  $("#login").hide();
+  $("#table button").hide();
+  $("#addImg").hide();
+  $("#zakaziOvde").click(function () {
+    $("#inputBoxes").toggle(1000);
+    $("#zakazivanje p").fadeToggle(2000);
+    $("#zakazi").toggle(1000);
+    $("#rezTermini").toggle(1000);
+    $("input ").val("");
+    $("select ").val("");
+  });
+
+  // Zakazivanje termina js
+
+  document.getElementById("zakazi").addEventListener("click", zakazivanje);
+  function zakazivanje() {
+    $("#demo").show();
+    var demo = document.getElementById("demo");
+    var datum = document.getElementById("datum");
+    var vreme = document.getElementById("vreme");
+    var broj = document.getElementById("broj");
+    var ime = document.getElementById("ime");
+    var email = document.getElementById("email");
+    var usluga = document.getElementById("usluga");
+    if (
+      datum.value !== "" &&
+      vreme.value !== "" &&
+      broj.value !== "" &&
+      usluga.value !== "" &&
+      email.value !== "" &&
+      ime.value !== ""
+    ) {
+      console.warn("sva polja su popunjena");
+
+      rez =
+        '<span id="rez">' +
+        "<h3>Rezervacija:</h3> <h3> Datum:  " +
+        datum.value +
+        "  - Vreme:  " +
+        vreme.value +
+        " </h3> </span>" +
+        '<div> <button id="potvrdi">Potvrdi</button> ' +
+        ' <button id="otkazi">Promeni unete podatke</button></div>';
+      demo.innerHTML = rez;
+      console.log([
+        datum.value,
+        vreme.value,
+        ime.value,
+        broj.value,
+        email.value,
+        usluga.value
+      ]);
+      document.getElementById("potvrdi").addEventListener("click", potvrdiRez);
+      document.getElementById("otkazi").addEventListener("click", otkaziRez);
+    } else {
+      console.warn("Nisu sva polja popunjena");
+      demo.innerHTML = "<h3>Popunite sva poqa!</h3>";
+    }
+  }
+
+  //potvrda rezervacije
+  function potvrdiRez() {
+    var demo = document.getElementById("demo");
+    rez = "<h3>Va{a rezervacija je potvr|ena !</h3>";
+    demo.innerHTML = rez;
+    $("#demo").fadeOut(2000);
+    console.log("Potvrdjeno");
+    var table = document.getElementById("table");
+    var tr = document.createElement("tr");
+    var tr2 = document.createElement("button");
+    var td1 = document.createElement("td");
+    var td2 = document.createElement("td");
+    var td3 = document.createElement("td");
+    var td4 = document.createElement("td");
+    var td5 = document.createElement("td");
+    var td6 = document.createElement("td");
+    var td7 = document.createElement("td");
+    var td8 = document.createElement("td");
+    var td9 = document.createElement("td");
+    var td10 = document.createElement("td");
+    var td11 = document.createElement("td");
+    var td12 = document.createElement("td");
+
+    console.log("sdfsdf");
+
+    td1.innerHTML = "<span id='green'>" + datum.value + "</span>";
+    td2.innerHTML = "<span id='green'>" + vreme.value + "</span>";
+    td3.innerHTML = ime.value;
+    td4.innerHTML = broj.value;
+    td5.innerHTML = email.value;
+    td6.innerHTML = "<span id='green'>" + usluga.value + "</span>";
+    td7.innerHTML = "<span id='red'>Datum: </span>";
+    td8.innerHTML = "<span id='red'>Vreme: </span>";
+    td9.innerHTML = "<span id='red'>Ime: </span>";
+    td10.innerHTML = "<span id='red'>Broj Telefona: </span>";
+    td11.innerHTML = "<span id='red'>Email Adresa: </span>";
+    td12.innerHTML = "<span id='red'>Usluga: </span>";
+    tr2.innerHTML = "Obri{i rezervaciju";
+    tr.appendChild(td7);
+    tr.appendChild(td1);
+    tr.appendChild(td8);
+    tr.appendChild(td2);
+    tr.appendChild(td12);
+    tr.appendChild(td6);
+    tr.appendChild(td9);
+    tr.appendChild(td3);
+    tr.appendChild(td10);
+    tr.appendChild(td4);
+    tr.appendChild(td11);
+    tr.appendChild(td5);
+
+    table.appendChild(tr);
+    table.appendChild(tr2);
+
+    $("#nemaRez").hide();
+    $("#table button").hide();
+    $("#addImg").hide();
+    $("#table button").click(function () {
+      $(this)
+        .prev()
+        .remove();
+      $(this).remove();
+    });
+  }
+
+  $("#rezTermini").click(function () {
+    $("#glavna").hide();
+    $("#oNama").hide();
+    $("#nasiRadovi").hide();
+    $("#cenovnik").hide();
+    $("#zakazivanje").hide();
+    $("#footer").hide();
+    $("#terminiDemo").show(1000);
+  });
+  // otkaz rezervacije
+  function otkaziRez() {
+    var demo = document.getElementById("demo");
+    rez = "<h3>Promena podataka.</h3>";
+    demo.innerHTML = rez;
+    $("#demo").fadeOut(2000);
+    console.log("Otkazano");
+  }
+
+});
